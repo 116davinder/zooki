3df69a4d6bff5105afaabcd77940a40a62435fa5
DAVINDER PAL <dpsangwal@gmail.com>
2020-03-22 12:31:16 +0200

    String to Dict based json generate
    added dict for disk stats as string results were not parsed by Splunk properly.
    added environment tag so Splunk can differentiate different environments.

--- zooki.py
+++ zooki.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# usage: python3 zooki.py /zookeeper /zookeeper/zookeeper-logs/
+# usage: python3 zooki.py /zookeeper /zookeeper/zookeeper-logs/ dev-env-zookeeper
 
 # This script suppose to export all zookeeper metric from one node and write to file
 # from where either splunk like tools can read it.
@@ -21,11 +21,14 @@ class zooki:
 
     def getStorageMetric(self):
         total, used, free = shutil.disk_usage(sys.argv[1])
-        _sMetric = "{" + '"@timestamp": "' + self.cTimeNow + '"' +\
-                    ',"command": ' + '"disk"' +\
-                    ',"totalInGB": ' + str(total // (2**30)) +\
-                    ',"usedInGB": ' + str(used // (2**30)) +\
-                    ',"freeInGB": ' + str(free // (2**30)) + "}"
+        _sMetric = {
+                    "@timestamp": self.cTimeNow,
+                    "command": "disk",
+                    "environment": sys.argv[3],
+                    "totalInGB": total // (2**30),
+                    "usedInGB":  used // (2**30),
+                    "freeInGB": free // (2**30) 
+                    }
         # print(json.dumps(_sMetric))
         return json.dumps(_sMetric)
 
@@ -33,6 +36,7 @@ class zooki:
         with request.urlopen( self.zHttpAddr + commandPath ) as f:
             _zMetric = json.loads(f.read().decode('utf-8'))
             _zMetric["@timestamp"] = self.cTimeNow
+            _zMetric["environment"] = sys.argv[3]
         # print(json.dumps(_zMetric))
         return json.dumps(_zMetric)
 
@@ -40,7 +44,7 @@ def main():
 
     commandPaths = ['connections', 'dump', 'leader', 'monitor',
                     'observers', 'server_stats', 'watches_by_path',
-                    'voting_view', 'watch_summary', 'zabstate', 'rouk']
+                    'voting_view', 'watch_summary', 'zabstate', 'ruok']
 
     z = zooki()
     with open(sys.argv[2] + "disk.out", "w") as zMetricFile:
