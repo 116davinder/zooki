708c9f7594e95509e57a02e26437f38f4e156106
DAVINDER PAL <dpsangwal@gmail.com>
2020-04-22 12:58:27 +0300

    Update zooki.py
    * Added status check for rest api calls
    * Added New Function for Monitor Command to reduce the json size
    * Removed Extra Commands which are not that useful

--- zooki.py
+++ zooki.py
@@ -27,24 +27,50 @@ class zooki:
                     "environment": sys.argv[3],
                     "totalInGB": total // (2**30),
                     "usedInGB":  used // (2**30),
-                    "freeInGB": free // (2**30) 
+                    "freeInGB": free // (2**30)
                     }
-        # print(json.dumps(_sMetric))
         return json.dumps(_sMetric)
 
     def getZMetric(self, commandPath):
         with request.urlopen( self.zHttpAddr + commandPath ) as f:
-            _zMetric = json.loads(f.read().decode('utf-8'))
-            _zMetric["@timestamp"] = self.cTimeNow
-            _zMetric["environment"] = sys.argv[3]
-        # print(json.dumps(_zMetric))
+            if f.status == 200:
+                _zMetric = json.loads(f.read().decode('utf-8'))
+                _zMetric["@timestamp"] = self.cTimeNow
+                _zMetric["environment"] = sys.argv[3]
+            else:
+                _zMetric = {}
+        return json.dumps(_zMetric)
+
+# json retruned by monitor is too big to be handled by splunk indexer
+# so separate function to reduce the json size
+    def getMonitorMetric(self):
+        with request.urlopen( self.zHttpAddr + "monitor" ) as f:
+            if f.status == 200:
+                _MM = json.loads(f.read().decode('utf-8'))
+                _zMetric = {
+                    "@timestamp": self.cTimeNow,
+                    "environment": sys.argv[3],
+                    "command": _MM["command"],
+                    "znode_count": _MM["znode_count"],
+                    "watch_count": _MM["watch_count"],
+                    "outstanding_requests": _MM["outstanding_requests"],
+                    "open_file_descriptor_count": _MM["open_file_descriptor_count"],
+                    "ephemerals_count": _MM["ephemerals_count"],
+                    "max_latency": _MM["max_latency"],
+                    "avg_latency": _MM["avg_latency"],
+                    "synced_followers": _MM["synced_followers"] if _MM["server_state"] == "leader" else 0,
+                    "pending_syncs": _MM["pending_syncs"] if _MM["server_state"] == "leader" else 0,
+                    "version": _MM["version"],
+                    "quorum_size": _MM["quorum_size"],
+                    "uptime": _MM["uptime"]
+                }
+            else:
+                _zMetric = {}
         return json.dumps(_zMetric)
 
 def main():
 
-    commandPaths = ['connections', 'dump', 'leader', 'monitor',
-                    'observers', 'server_stats', 'watches_by_path',
-                    'voting_view', 'watch_summary', 'zabstate', 'ruok']
+    commandPaths = ['connections', 'leader', 'watch_summary']
 
     z = zooki()
     with open(sys.argv[2] + "disk.out", "w") as zMetricFile:
@@ -54,4 +80,6 @@ def main():
         with open(sys.argv[2] + command + ".out", "w") as zMetricFile:
             zMetricFile.write(z.getZMetric(command))
 
+    with open(sys.argv[2] + "monitor.out", "w") as zMetricFile:
+        zMetricFile.write(z.getMonitorMetric())
 main()
